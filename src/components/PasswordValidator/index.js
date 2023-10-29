e0743f512ed8e37a98dee9459fae3de02c589f6b
SachinBalagam <balagamsachin337@gmail.com>
2023-10-29 06:06:12 +0000

    Final Commit

--- src/components/PasswordValidator/index.js
+++ src/components/PasswordValidator/index.js
@@ -1,1 +1,44 @@
 // Write your code here
+import {useState} from 'react'
+import {
+  AppContainer,
+  ValidatorCard,
+  Heading,
+  Description,
+  CustomInput,
+  ValidatorText,
+} from './styledComponents'
+
+const PasswordValidator = () => {
+  const [isValid, setIsValid] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const onPasswordChange = event => {
+    setPassword(event.target.value)
+    if (password.length >= 7) {
+      return setIsValid(true)
+    }
+    return setIsValid(false)
+  }
+
+  return (
+    <AppContainer>
+      <ValidatorCard>
+        <Heading>Password Validator</Heading>
+        <Description>Check how strong and secure is your password</Description>
+        <CustomInput
+          type="password"
+          onChange={onPasswordChange}
+          value={password}
+        />
+        {!isValid && (
+          <ValidatorText>
+            Your password must be at least 8 characters
+          </ValidatorText>
+        )}
+      </ValidatorCard>
+    </AppContainer>
+  )
+}
+
+export default PasswordValidator
--- src/components/PasswordValidator/styledComponents.js
+++ src/components/PasswordValidator/styledComponents.js
@@ -1,1 +1,39 @@
 // Style your elements here
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #383a4e;
+`
+export const ValidatorCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #475569;
+  padding: 20px;
+  border-radius: 10px;
+  width: 500px;
+  height: 400px;
+`
+export const Heading = styled.h1`
+  color: #ffffff;
+  font-size: 40px;
+  font-family: 'Roboto';
+`
+export const Description = styled.p`
+  color: #f8fafc;
+  font-family: 'Roboto';
+`
+export const CustomInput = styled.input`
+  height: 35px;
+  width: 400px;
+`
+export const ValidatorText = styled.p`
+  color: #ef4444;
+  font-family: 'Roboto';
+`
